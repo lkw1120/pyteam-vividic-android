00eed7a1242734da58e6ff2a869ec5fb69c80cce
lkw1120 <lkwkang@gmail.com>
2020-07-26 17:17:23 +0900

    Feat : 메인 페이지에 슬라이딩메뉴 추가

--- app/src/main/java/com/pyteam/vividic/ui/MainFragment.kt
+++ app/src/main/java/com/pyteam/vividic/ui/MainFragment.kt
@@ -2,16 +2,21 @@ package com.pyteam.vividic.ui
 
 import android.os.Bundle
 import android.view.*
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.pyteam.vividic.viewmodel.MainViewModel
 import com.pyteam.vividic.R
 import com.pyteam.vividic.databinding.FragmentMainBinding
 import com.pyteam.vividic.ui.adapter.ListItemDecoration
 import com.pyteam.vividic.ui.adapter.MovieListAdapter
 import com.pyteam.vividic.ui.adapter.TvShowListAdapter
+import com.pyteam.vividic.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
@@ -29,8 +34,23 @@ class MainFragment : Fragment() {
             model = viewModel
             lifecycleOwner = viewLifecycleOwner
 
-            (activity as AppCompatActivity).setSupportActionBar(toolbar)
-            setHasOptionsMenu(true)
+            (activity as AppCompatActivity).apply {
+                setSupportActionBar(toolbar)
+                supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+                supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_menu_24px)
+                setHasOptionsMenu(true)
+
+            }
+
+            mainLayout.addDrawerListener(
+                ActionBarDrawerToggle(
+                    requireActivity(),
+                    mainLayout,
+                    toolbar,
+                    R.string.app_name,
+                    R.string.app_name
+                )
+            )
 
             val moviesNowPlayingAdapter =
                 MovieListAdapter(object : MovieListAdapter.OnItemClickListener {
@@ -154,5 +174,16 @@ class MainFragment : Fragment() {
         }
     }
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback {
+            if (binding.mainLayout.isDrawerOpen(GravityCompat.START)) {
+                binding.mainLayout.closeDrawer(GravityCompat.START)
+            } else {
+                if (!findNavController().popBackStack()) {
+                    requireActivity().finish()
+                }
+            }
+        }
+    }
 }
